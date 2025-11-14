3618112580e3e8a7446d3453d98e1d3561abba7a
Benji <benji@caddell.de>
2025-11-14 05:53:57 +0100

    Added basic socket io server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,17 @@
-import { port } from "./User.ts";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-console.log("I'm server.ts!", port);
+const PORT: number = 3000;
+
+const httpServer = createServer();
+const io = new Server(
+    httpServer,
+    { cors: { origin: "https://hoppscotch.io" } }
+);
+
+io.on('connection', (socket) => {
+    // console.log(socket.request.headers);
+    socket.emit("message", "bananas", () => {console.log("yummy")});
+});
+
+httpServer.listen(PORT);
